44bbac7c523a77d37dfd1b22f431c74125c680c1
Alex <Alex@Mac-mini-Alex.local>
2012-09-27 16:27:52 +0400

    Status API

--- clojure/src/CassandraLibrary.clj
+++ clojure/src/CassandraLibrary.clj
@@ -4,24 +4,48 @@
 ;;init value
 (set! (. io.cassandra.sdk.constants.APIConstants API_VERSION) "1")
 
+; Status Message API
+(defn isOK [StatusMessageObject]
+  (.isOk StatusMessageObject)                               ;return bool
+ )
+(defn getMessageObj [StatusMessageObject]
+  (.getMessage StatusMessageObject)                         ;return string with message
+  )
+(defn getStatusObj [StatusMessageObject]
+  (.getStatus StatusMessageObject)                          ;return string with status
+  )
+(defn getDetailObj [StatusMessageObject]
+  (.getDetail StatusMessageObject)                          ;return string with detail
+  )
+(defn getTtlObj [StatusMessageObject]
+  (.getTtl StatusMessageObject)                             ;return integer
+  )
+(defn getErrorObj [StatusMessageObject]
+  (.getError StatusMessageObject)                           ;return string with Error
+  )
+
+(defn getAllInfoStatus [StatusMessageObject]
+  (.concat
+    (.concat
+      (.concat
+        (.concat (getStatusObj StatusMessageObject) "\n")
+        (getMessageObj StatusMessageObject)) "\n")
+    (getDetailObj StatusMessageObject)
+  )                                                          ;return string with full status
+ )
+
 ; KeySpace API
 (import 'io.cassandra.sdk.keyspace.KeyspaceAPI)
 (defn deleteKeyspace [keySpaceName accountId token]
   (def keySpace
     (KeyspaceAPI. io.cassandra.sdk.constants.APIConstants/API_URL token accountId))
-
-  (println
-    (.getDetail
-      (.deleteKeyspace keySpace keySpaceName)))
+  (.deleteKeyspace keySpace keySpaceName)                   ;return StatusMessageObject
   )
 
 (defn createKeyspace [keySpaceName accountId token]
   (def keySpace
     (KeyspaceAPI. io.cassandra.sdk.constants.APIConstants/API_URL token accountId))
-
-  (println
-    (.getDetail
-      (.createKeyspace keySpace keySpaceName)))
+  (.createKeyspace keySpace keySpaceName)                   ;return StatusMessageObject
   )
 
 ;ColumnFamily API
@@ -29,17 +53,15 @@
 (defn createColumnFamily [keyspaceName columnFamilyName comparatorType accountId token]
   (def columnFamily
     (ColumnFamilyAPI. io.cassandra.sdk.constants.APIConstants/API_URL token accountId))
-
-  (println (.getDetail
-             (.createColumnFamily columnFamily keyspaceName columnFamilyName comparatorType)))
+  (.createColumnFamily columnFamily keyspaceName columnFamilyName comparatorType)
+                                                            ;return StatusMessageObject
   )
 
 (defn deleteColumnFamily [keyspaceName columnFamilyName accountId token]
   (def columnFamily
     (ColumnFamilyAPI. io.cassandra.sdk.constants.APIConstants/API_URL token accountId))
-  (println
-    (.getDetail
-      (.deleteColumnFamily columnFamily keyspaceName columnFamilyName)))
+  (.deleteColumnFamily columnFamily keyspaceName columnFamilyName)
+                                                            ;return StatusMessageObject
   )
 
 
@@ -56,7 +78,7 @@
  )
 
 (defn getSortedby [columnFamilyObject]
-  (.getSortedby (.getColumnfamily columnFamilyObject))          ;return string with name columnfamily
+  (.getSortedby (.getColumnfamily columnFamilyObject))      ;return string with name columnfamily
   )
 
 (defn getColumnFamiliesName [keyspaceName accountId token]
@@ -79,11 +101,11 @@
   (vec returnVector)                                         ;return 2d vector with ColumnFamily name and Sorted by
   )
 
-;;(createKeyspace "base2" accountIdTemp tokenTemp)
-;;(deleteKeyspace "base2" accountIdTemp tokenTemp)
-;;(createColumnFamily "base2" "tab5" "UTF8Type" accountIdTemp tokenTemp)
-;;(deleteColumnFamily "base2" "tab1" accountIdTemp tokenTemp)
-(println (getColumnFamiliesNameAndSortedBy "base1" accountIdTemp tokenTemp))
+;;(println (getAllInfoStatus (createKeyspace "base2" accountIdTemp tokenTemp)))
+;;(println (getAllInfoStatus (deleteKeyspace "base2" accountIdTemp tokenTemp)))
+;;(println (getAllInfoStatus(createColumnFamily "base2" "tab7" "UTF8Type" accountIdTemp tokenTemp)))
+;;(println (getAllInfoStatus(deleteColumnFamily "base2" "tab6" accountIdTemp tokenTemp)))
+(println (getColumnFamiliesNameAndSortedBy "base2" accountIdTemp tokenTemp))
 
 
 
