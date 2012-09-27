dc288b3b8775c298a26895f010f3626613c176fa
Alex <Alex@Mac-mini-Alex.local>
2012-09-27 14:24:08 +0400

    get ColumnFamily Objects and ColumnFamilies Name functions

--- clojure/src/CassandraLibrary.clj
+++ clojure/src/CassandraLibrary.clj
@@ -37,35 +37,40 @@
 (defn deleteColumnFamily [keyspaceName columnFamilyName accountId token]
   (def columnFamily
     (ColumnFamilyAPI. io.cassandra.sdk.constants.APIConstants/API_URL token accountId))
-
   (println
     (.getDetail
       (.deleteColumnFamily columnFamily keyspaceName columnFamilyName)))
   )
 
-(defn getColumnFamilies [keyspaceName accountId token]
 
+(defn getColumnFamilyObjects [keyspaceName accountId token]
   (def columnFamily
     (ColumnFamilyAPI. io.cassandra.sdk.constants.APIConstants/API_URL token accountId))
-
-  (def colunmFamilyArray
     (to-array
-      (.getColumnfamilies (.getColumnFamilies columnFamily keyspaceName ))))
-
-  (def returnVector (vec (object-array 0)))
-  (areduce colunmFamilyArray i ret ()  (def returnVector (conj returnVector (.getName (.getColumnfamily(aget colunmFamilyArray i))))))
-
-  (vec returnVector)     ;return vector ColumnFamili name
+      (.getColumnfamilies
+        (.getColumnFamilies columnFamily keyspaceName )))   ;return array with columnfamily objects
 )
 
+(defn getNameColumnFamily [columnFamilyObject]
+  (.getName (.getColumnfamily columnFamilyObject))          ;return string with name columnfamily
+ )
 
-
+(defn getColumnFamiliesName [keyspaceName accountId token]
+  (def colunmFamilyArray
+    (getColumnFamilyObjects keyspaceName accountId token))
+  (def returnVector (vec (object-array 0)))
+  (areduce colunmFamilyArray i ret ()  (def returnVector
+                                         (conj returnVector
+                                           (getNameColumnFamily
+                                             (aget colunmFamilyArray i)))))
+  (vec returnVector)                                         ;return vector ColumnFamili name
+ )
 
 ;;(createKeyspace "base2" accountIdTemp tokenTemp)
 ;;(deleteKeyspace "base2" accountIdTemp tokenTemp)
 ;;(createColumnFamily "base2" "tab5" "UTF8Type" accountIdTemp tokenTemp)
 ;;(deleteColumnFamily "base2" "tab1" accountIdTemp tokenTemp)
-(println (getColumnFamilies "base2" accountIdTemp tokenTemp))
+(println (getColumnFamiliesName "base2" accountIdTemp tokenTemp))
 
 
 
